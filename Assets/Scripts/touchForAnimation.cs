83f8b9906467051dde6e1a35db53f32960146d0d
Birgitte <birgitte.wintersto@outlook.com>
2020-06-08 23:19:25 +0200

    Touch to Jump + audio

---
+++ Assets/Scripts/touchForAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchForAnimation : MonoBehaviour
+{
+    Animator _animator;
+
+    bool isScreenTouched = false;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        isScreenTouched = true;
+                    }
+                    break;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animator.SetBool("isScreenTouched", true);
+            Invoke("TurnOffAnimatorTool", .01f);
+        }
+    }
+
+    void TurnOffAnimatorTool()
+    {
+        _animator.SetBool("isScreenTouched", false);
+    }
+        
+
+}
